772b0246e68201a92597eb6cf9dffc887ac7fe23
Nwosu Chisom Martin <150328063+ChisomNwosu@users.noreply.github.com>
2023-12-18 19:12:36 +0100

    initial commit

--- Covid Portfolio Project - Data Exploration v2.sql
+++ Covid Portfolio Project - Data Exploration v2.sql
@@ -123,16 +123,3 @@ Select *, (RollingPeopleVaccinated/population)*100
 From #percentpopulationVaccinated
 
 
---Creating View to store data for later visualization
-
-Create View percentpopulationvaccinated as
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
- , SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location,
-    dea.Date) as RollingPeopleVaccinated
-From PortfolioProject..CovidDeaths$ dea
-join PortfolioProject..CovidVaccinations$ vac
-  On dea.location = vac.location
-  and dea.date = vac.date
-where dea.continent is not null
---order by 2,3
-
